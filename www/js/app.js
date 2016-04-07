cd1118b33364a8cd85c3e46d35400a42a13278ab
Burhan <bern.3759@yahoo.com>
2016-04-07 21:16:22 +0800

    Added file and modify the Main page and Chart Page

--- www/js/app.js
+++ www/js/app.js
@@ -59,25 +59,6 @@ angular.module('starter', ['ionic','ionic.service.core','ngCordova', 'starter.co
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
   .state('tab.account', {
     url: '/ChartAnalysis',
     views: {
