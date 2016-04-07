a84cd8a79a47ff7551aca8837153b1a8bcb9b618
Burhan <bern.3759@yahoo.com>
2016-04-07 22:44:31 +0800

    Add lib

--- www/js/timer.js
+++ www/js/timer.js
@@ -11,20 +11,5 @@ angular.module('starter.timer', ['chart.js'])
     $scope.myStopwatch.start();
 
     var d = $scope.myStopwatch;
-
-    console.log(d.data.seconds);
-    console.log(d.rec);
-    console.log(d.labels[0]);
-    console.log(d.points[0][0]);
-
-
-
-    // $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    // $scope.series = ['Series A', 'Series B'];
-    // $scope.data = [
-    //     [65, 59, 80, 81, 56, 55, 40],
-    //     [28, 48, 40, 19, 86, 27, 90]
-    // ]
-
     
 })
